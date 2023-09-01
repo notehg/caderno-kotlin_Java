ec88558dc7d45d4b2a9c8b12769d5bc4ae05a4e0
notehg <110551666+notehg@users.noreply.github.com>
2023-09-01 11:37:56 -0300

    Create Helloword.kt

---
+++ Helloword.kt
@@ -0,0 +1,19 @@
+fun main(){
+  var what = "ah shit, here we go again"
+  val nu = ",energumeno"
+  
+  ///primeiro codigo em kotlin
+  println("Mensagem:");
+  ///println escreve em linhas diferentes
+  println("Vamos programar em kotlin");
+  println("Vamos lá");
+  println(nu);
+  println("5+2");
+  ///aqui esta uma soma
+  println(5+2);
+  println(what);
+  print("interessante");
+  ///print escreve em mesma linha
+  print(" como você faz isso?");
+
+}
